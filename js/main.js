e614c802ac58de85e258b0a9d50fecf5e6c32ef3
munseesoeraisha <munseesoeraisha2@gmail.com>
2026-05-22 12:25:13 -0700

    menu buttons

--- js/main.js
+++ js/main.js
@@ -20,3 +20,16 @@ closeSidebar.onclick = function(){
     contactSidebar.classList.remove("show");
 
 };
+
+const menuIcon =
+document.querySelector(".menu-icon");
+
+const navLinks =
+document.querySelector(".nav-links");
+
+
+menuIcon.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
